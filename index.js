2cffac037bf17711d04dcd460afe45d4536eb0c5
Unknown <cougoulat.aymrick@gmail.com>
2021-01-06 15:34:03 +0100

    feat cors

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const emergencyRoutes = require('./routes/emergency/routes.js');
 const simulatorRoutes = require('./routes/simulator/routes.js');
 
 const express = require('express');
+const cors = require('cors');
 
-// SET UP Express
+// SET UP Express & Librairy
 const app = express();
+app.use(cors());
 
 //Load HTTP module
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.use('/emergency-manager', emergencyRoutes);
-app.use('/simulator', simulatorRoutes);
+app.use('/emergency-manager-api', emergencyRoutes);
+app.use('/simulator-api', simulatorRoutes);
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 app.listen(port, hostname, () => {
