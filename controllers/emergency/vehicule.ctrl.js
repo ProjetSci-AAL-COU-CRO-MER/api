445d87d8f1a0049f3f402a74a9823e505e1f0036
adrienCrosio <adrien.crosio@cpe.fr>
2021-01-14 00:30:13 +0100

    change little error

--- controllers/emergency/vehicule.ctrl.js
+++ controllers/emergency/vehicule.ctrl.js
@@ -2,6 +2,7 @@ const db = require('../../config/db.emergency');
 const table = 'vehicule';
 const table_type = 'vehicule_type';
 const table_etat = 'vehicule_etat';
+const table_etablissement = 'vehicule_etat';
 
 class VehiculeController {
 
@@ -9,7 +10,7 @@ class VehiculeController {
 
     getAll(req, res, next) {
         db
-        .query(`SELECT v.nom, vt.libelle as type_vehicule, ve.libelle as etat_vehicule, v.latitude, v.longitude FROM ${table} v LEFT JOIN ${table_type} vt ON vt.id = v.id_vehicule_type LEFT JOIN ${table_etat} ve ON ve.id = v.id_vehicule_etat`)
+        .query(`SELECT v.nom, et.id as id_etablissement, vt.libelle as type_vehicule, ve.libelle as etat_vehicule, v.latitude, v.longitude , v.id FROM ${table} v LEFT JOIN ${table_type} vt ON vt.id = v.id_vehicule_type LEFT JOIN ${table_etat} ve ON ve.id = v.id_vehicule_etat INNER JOIN ${table_etablissement} as et on v.id_etablissement=et.id `)
         .then(e => res.send(e.rows))
         .catch(e => console.error(e.stack));
     }
